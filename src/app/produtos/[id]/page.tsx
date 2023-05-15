8bc146ebd47b92ad95ea472f879a654fa093de76
{ Walyson } <walysonmoura222@gmail.com>
2023-05-15 18:44:52 +0000

    new

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -9,13 +9,17 @@ import SingleProduct from '../components/SingleProduct';
 import { useRouter } from 'next/router';
 
 
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { Suspense } from "react";
 
 
 export async function generateStaticParams() {
   const WOOCOMMERCE_GRAPHQL_URL = process.env.WOOCOMMERCE_GRAPHQL_URL
-  const products = await fetch(`${WOOCOMMERCE_GRAPHQL_URL}/products`).then((res) => res.json());
+  const products = await fetch(`${WOOCOMMERCE_GRAPHQL_URL}/products`, {
+    next: {
+      revalidate: 60 * 2
+    }
+  }).then((res) => res.json());
 
   return products.map((product: { id: any; }) => ({
     id: product.id,
@@ -29,11 +33,38 @@ interface ProductsProps {
   };
 }
 
-export async function generateMetadate({ params }: ProductsProps) {
+function formatSlug(slug: string): string {
+  const encodedSlug = encodeURIComponent(slug);
+  const decodedSlug = encodedSlug.replace(/-/g, ' ');
+
+  return decodedSlug;
+}
+
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+ 
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent?: ResolvingMetadata,
+): Promise<Metadata> {
+
+  
+  const formattedSlug = await formatSlug(params.id)
+ 
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+ 
   return {
-    title: params.id,
+    title: formattedSlug,
+    openGraph: {
+      images: ['/some-specific-page-image.jpg', ...previousImages],
+    },
   };
 }
+ 
 
 function SkeletonProduct() {
   return (
